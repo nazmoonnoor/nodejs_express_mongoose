2d35d0792d060efe528127b76203f057b8cc70d7
nazmoonnoor <nazmoonnoor@gmail.com>
2022-06-25 16:36:49 +0600

    Update app.routes.ts

--- src/routes/app.routes.ts
+++ src/routes/app.routes.ts
@@ -5,27 +5,8 @@ import { use } from "../middleware/errorHandler";
 const router = express.Router();
 const controller = new AppController();
 
-// Routes
-/**
- * @swagger
- * /healthcheck:
- *  get:
- *    description: Use to check if server is running
- *    responses:
- *      '200':
- *        description: A successful response
- */
 router.get("/healthcheck", controller.healthcheck);
 
-/**
- * @swagger
- * /seed:
- *  get:
- *    description: Use to seed the test data
- *    responses:
- *      '200':
- *        description: A successful response
- */
 router.get("/seed", use(controller.seed));
 
 export default router;
